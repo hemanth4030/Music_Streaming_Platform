0dc589816083b3aeae1c494d768916b7e6fcaf3f
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-01-13 16:41:13 +0530

    deleted vite css and started the nav bar of music_platform taking the reference of spotify

--- music_platform/src/App.jsx
+++ music_platform/src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-
+import './index.css'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   <div className='container-fluid'>
+    <div className='row'>
+      <div className='col-12'>
+        <nav className='navbar navbar-expand-lg navbar-black bg-dark'>
+          <img src='https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png' alt='logo' className='logo' />
+          <ul className='navbar-nav'> 
+            <li className='nav-item'>
+              <a href='#' className='nav-link'>Home</a>
+            </li>
+            <li className='nav-item'>
+              <a href='#' className='nav-link'>About</a>
+            </li>
+            <li className='nav-item'>
+              <a href='#' className='nav-link'>Contact</a>
+            </li> 
+          </ul>
+        </nav>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+   </div>
   )
 }
 
