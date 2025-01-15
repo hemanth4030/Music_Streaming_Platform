0cff94f12e141f2e1853418e6123f7f22a60c71a
R.V.Chandra Sekhara Raju <rvcsraju@gmail.com>
2025-01-15 11:41:47 +0530

    Update App.jsx and added the library in the nav bar

--- music_platform/src/App.jsx
+++ music_platform/src/App.jsx
@@ -15,6 +15,9 @@ function App() {
               <a href='#' className='nav-link'>Home</a>
             </li>
             <li className='nav-item'>
+              <a href='#' className='nav-link'>Library</a>
+            </li>
+             <li className='nav-item'>
               <a href='#' className='nav-link'>About</a>
             </li>
             <li className='nav-item'>
