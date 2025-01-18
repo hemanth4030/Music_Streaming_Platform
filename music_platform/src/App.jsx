e437620970a510cc4181e9559f6f4fb44509ae7b
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-01-18 16:32:50 +0530

    changed to the normal one and tried to complete thenav bar

--- music_platform/src/App.jsx
+++ music_platform/src/App.jsx
@@ -8,24 +8,61 @@ function App() {
    <div className='container-fluid'>
     <div className='row'>
       <div className='col-12'>
-        <nav className='navbar navbar-expand-lg navbar-black bg-dark'>
-          <img src='https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png' alt='logo' className='logo' />
-          <ul className='navbar-nav'> 
-            <li className='nav-item'>
-              <a href='#' className='nav-link'>Home</a>
-            </li>
-            <li className='nav-item'>
-              <a href='#' className='nav-link'>Library</a>
-            </li>
-             <li className='nav-item'>
-              <a href='#' className='nav-link'>About</a>
-            </li>
-            <li className='nav-item'>
-              <a href='#' className='nav-link'>Contact</a>
-            </li> 
-          </ul>
-        </nav>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary col-12">
+          <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>       
+            <a className="navbar-brand" href="#">
+              <h1 className="com_name">Muzic</h1>
+            </a>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-1">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Library</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+          
+             </div>  
+        </div>
+        <div className="d-flex flex-row justify-content-center">
+          <div>
+            <button className="button_prof" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <i className="fa-solid fa-magnifying-glass icon"></i>
+            </button>
+          </div>
+          <div>
+            <button className="button_prof m-auto">
+            <i className="fa-solid fa-bell icon"></i>
+            </button>
+          </div>
+          <div>
+            <button className="button_prof m-auto">
+            <i className="fa-solid fa-user icon"></i>
+            </button>
+          </div>
+        </div>
+      </nav>    
       </div>
+      <div className="modal fade col-12" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-body">
+                <form className="d-flex flex-row justify-content-start" role="search">
+                  <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
    </div>
   )
