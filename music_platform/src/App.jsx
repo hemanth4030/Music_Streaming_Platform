0f6edfb4aee0d2c251e508de8530b44a204f0247
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-01-18 17:16:41 +0530

    updated in th emain branch only , change the background to black and logos to the grey

--- music_platform/src/App.jsx
+++ music_platform/src/App.jsx
@@ -2,69 +2,53 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import './index.css'
-function App() {
 
+function App() {
   return (
-   <div className='container-fluid'>
-    <div className='row'>
-      <div className='col-12'>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary col-12">
-          <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>       
-            <a className="navbar-brand" href="#">
-              <h1 className="com_name">Muzic</h1>
-            </a>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-1">
-                  <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Library</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-          
-             </div>  
-        </div>
-        <div className="d-flex flex-row justify-content-center">
-          <div>
-            <button className="button_prof" data-bs-toggle="modal" data-bs-target="#exampleModal">
-              <i className="fa-solid fa-magnifying-glass icon"></i>
-            </button>
-          </div>
-          <div>
-            <button className="button_prof m-auto">
-            <i className="fa-solid fa-bell icon"></i>
-            </button>
-          </div>
-          <div>
-            <button className="button_prof m-auto">
-            <i className="fa-solid fa-user icon"></i>
-            </button>
+    <nav className="navbar navbar-expand-lg bg-black bg-body-dark col-12">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="#">
+          <h1 className="d-flex flex-row justify-content-centre">
+            <img src="Icons_music_platform/icon_logo.png" alt="logo" className="logo" />
+          </h1>
+        </a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="navbar-nav me-auto mb-2 mb-lg-1">
+            <li className="nav-item">
+              <a className="nav-link active" style={{ color: 'white' }} aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" style={{ color: 'white' }} aria-current="page" href="#">Library</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" style={{ color: 'white' }} href="#">Link</a>
+            </li>
           </div>
         </div>
-      </nav>    
+        <div>
+        </div>  
       </div>
-      <div className="modal fade col-12" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-body">
-                <form className="d-flex flex-row justify-content-start" role="search">
-                  <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                </form>
-              </div>
-            </div>
-          </div>
+      <div className="d-flex flex-row justify-content-center">
+        <div>
+          <button className="button_prof" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <i className="fa-solid fa-magnifying-glass icon" style={{ color: 'grey' }}></i>
+          </button>
         </div>
-    </div>
-   </div>
+        <div>
+          <button className="button_prof m-auto">
+            <i className="fa-solid fa-bell icon" style={{ color: 'grey' }}></i>
+          </button>
+        </div>
+        <div>
+          <button className="button_prof m-auto">
+            <i className="fa-solid fa-user icon" style={{ color: 'grey' }}></i>
+          </button>
+        </div>
+      </div>
+    </nav>
   )
 }
 
